2786fc9fef40f947648b6922bf2fdb445b994b52
softwaremakassar <softwaremakassar@gmail.com>
2021-08-17 20:27:47 +0800

    build: update mock with function module

---
+++ tests/src/mocks/getAllProducts.mock.js
@@ -0,0 +1,12 @@
+module.exports = async () => {
+  const users = [
+    {
+      id: 1,
+      name: "Alex",
+      gender: "male",
+      address: { name: "St. Petersburg, New York" },
+    },
+  ];
+
+  return users;
+};
